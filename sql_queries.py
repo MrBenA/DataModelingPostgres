15a9025d9c29d81f8473ca5db2dacb5b7072a48a
Ben <mrbena77@gmail.com>
2020-12-30 16:00:01 +0000

    re-ordered create table statements user_id datatype correction Added 'time' table primary key Added 'songplay' foreign keys 'user' table on conflict changed to UPDATE

--- sql_queries.py
+++ sql_queries.py
@@ -8,20 +8,6 @@ time_table_drop = """DROP TABLE IF EXISTS time"""
 
 # CREATE TABLES
 
-songplay_table_create = ("""
-    CREATE TABLE IF NOT EXISTS songplays (
-        songplay_id SERIAL PRIMARY KEY,
-        start_time TIMESTAMP NOT NULL,
-        user_id VARCHAR NOT NULL,
-        level VARCHAR NOT NULL,
-        song_id VARCHAR,
-        artist_id VARCHAR,
-        session_id INT NOT NULL,
-        location VARCHAR,
-        user_agent VARCHAR
-    );
-""")
-
 user_table_create = ("""
     CREATE TABLE IF NOT EXISTS users (
         user_id INT PRIMARY KEY,
@@ -54,7 +40,7 @@ artist_table_create = ("""
 
 time_table_create = ("""
     CREATE TABLE IF NOT EXISTS time (
-        start_time TIMESTAMP NOT NULL,
+        start_time TIMESTAMP PRIMARY KEY,
         hour INT NOT NULL,
         day INT NOT NULL,
         week INT NOT NULL,
@@ -64,18 +50,30 @@ time_table_create = ("""
     );
 """)
 
-# INSERT RECORDS
-
-songplay_table_insert = ("""
-    INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+songplay_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songplays (
+        songplay_id SERIAL PRIMARY KEY,
+        start_time TIMESTAMP NOT NULL,
+        user_id INT NOT NULL,
+        level VARCHAR NOT NULL,
+        song_id VARCHAR,
+        artist_id VARCHAR,
+        session_id INT NOT NULL,
+        location VARCHAR,
+        user_agent VARCHAR,
+        FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+        FOREIGN KEY (song_id) REFERENCES songs(song_id) ON DELETE CASCADE,
+        FOREIGN KEY (artist_id) REFERENCES artists(artist_id) ON DELETE CASCADE
+    );
 """)
+
+# INSERT RECORDS
 
 user_table_insert = ("""
     INSERT INTO users (user_id, first_name, last_name, gender, level)
         VALUES (%s, %s, %s, %s, %s)
         ON CONFLICT (user_id)
-        DO NOTHING
+        DO UPDATE SET level=EXCLUDED.level
 """)
 
 song_table_insert = ("""
@@ -95,6 +93,13 @@ artist_table_insert = ("""
 time_table_insert = ("""
     INSERT INTO time (start_time, hour, day, week, month, year, weekday)
         VALUES (%s, %s, %s, %s, %s, %s, %s)
+        ON CONFLICT (start_time)
+        DO NOTHING
+""")
+
+songplay_table_insert = ("""
+    INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
 """)
 
 # FIND SONGS
@@ -109,6 +114,6 @@ song_select = ("""
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create,
-                        time_table_create]
+create_table_queries = [user_table_create, song_table_create, artist_table_create,
+                        time_table_create, songplay_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
